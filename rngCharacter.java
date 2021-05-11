6f987a30b139c57be25a82d06914eb764f303f25
kyx <48016628+Cherecho@users.noreply.github.com>
2021-05-11 13:52:28 +0200

    Update rngCharacter.java

--- rngCharacter.java
+++ rngCharacter.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package classes;
 
 import java.util.*;
 import javax.swing.JOptionPane;
-/**
- *
- * @author Campus FP
- */
+
 public class rngCharacter {
     public static void main(String[] args) {
         
